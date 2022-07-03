7e32f2844c07a0b19ab394a6065af09ad5943054
Elteyab Hassan <Mr.elteb@gmail.com>
2022-07-03 19:46:31 +0200

    update

--- src/app/components/hero/hero.component.ts
+++ src/app/components/hero/hero.component.ts
@@ -9,20 +9,6 @@ import { Component, OnInit } from '@angular/core';
 export class HeroComponent implements OnInit {
 
   constructor() {
-    const options = {
-      strings: ['Innovation.', 'Discovery.'],
-      stringsElement: '#typed',
-      // strings: ['<i>First</i> sentence.', '&amp; a second sentence.'],
-      typeSpeed: 100,
-      backSpeed: 100,
-      showCursor: true,
-      cursorChar: '|',
-      loop: true,
-
-    };
-
-    // const typed = new Typed('typed', options);
-
   }
 
   ngOnInit(): void {
